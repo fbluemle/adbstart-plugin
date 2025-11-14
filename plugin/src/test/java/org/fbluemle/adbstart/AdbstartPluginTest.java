84e23545bfeee8ea453f09fcba43725b6215c653
Frieder Bluemle <frieder.bluemle@gmail.com>
2025-11-13 19:51:55 -0800

    Fix activity component name with suffix

--- plugin/src/test/java/org/fbluemle/adbstart/AdbstartPluginTest.java
+++ plugin/src/test/java/org/fbluemle/adbstart/AdbstartPluginTest.java
@@ -13,4 +13,33 @@ public class AdbstartPluginTest {
         project.getPlugins().apply("org.fbluemle.adbstart");
         assertNotNull(project.getExtensions().findByName("adbStart"));
     }
+
+    @Test
+    public void extensionHasDefaultValues() {
+        Project project = ProjectBuilder.builder().build();
+        project.getPlugins().apply("org.fbluemle.adbstart");
+        AdbStartExtension ext = project.getExtensions().getByType(AdbStartExtension.class);
+
+        assertEquals(".MainActivity", ext.getActivity());
+        assertEquals("", ext.getExtraAmArgs());
+        assertEquals("adb", ext.getAdbPath());
+        assertNull(ext.getDeviceSerial());
+    }
+
+    @Test
+    public void extensionCanBeConfigured() {
+        Project project = ProjectBuilder.builder().build();
+        project.getPlugins().apply("org.fbluemle.adbstart");
+        AdbStartExtension ext = project.getExtensions().getByType(AdbStartExtension.class);
+
+        ext.setActivity(".CustomActivity");
+        ext.setDeviceSerial("emulator-5554");
+        ext.setExtraAmArgs("-W");
+        ext.setAdbPath("/custom/path/to/adb");
+
+        assertEquals(".CustomActivity", ext.getActivity());
+        assertEquals("emulator-5554", ext.getDeviceSerial());
+        assertEquals("-W", ext.getExtraAmArgs());
+        assertEquals("/custom/path/to/adb", ext.getAdbPath());
+    }
 }
